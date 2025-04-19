a5388ac36eaea17d50180a3b03c8ba3ed928d74a
Avila Brian Leonel <61570098+avilabl@users.noreply.github.com>
2025-04-19 20:57:14 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const pool = require('./db');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+// ✅ Obtener todos los empleados
+app.get('/empleados', async (req, res) => {
+  try {
+    const [rows] = await pool.query('SELECT * FROM empleados');
+    res.json(rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.get('/roles', async (req, res) => {
+    try {
+      const [rows] = await pool.query('SELECT * FROM roles');
+      res.json(rows);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
+
+// ✅ Insertar un nuevo empleado
+app.post('/empleados', async (req, res) => {
+  const { nombreEmpleado, apellidoEmpleado, idRol, fechaIngreso } = req.body;
+  try {
+    const [result] = await pool.query(
+      'INSERT INTO empleados (nombreEmpleado, apellidoEmpleado, idRol, fechaIngreso) VALUES (?, ?, ?, ?)',
+      [nombreEmpleado, apellidoEmpleado, idRol, fechaIngreso]
+    );
+    res.json({ idEmpleado: result.insertId });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
+app.post('/roles', async (req, res) => {
+    const { nombreRol } = req.body;
+    try {
+      const [result] = await pool.query(
+        'INSERT INTO empleados (nombreRol) VALUES (?)',
+        [nombreRol]
+      );
+      res.json({ idRol: result.insertId });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
+
+app.listen(PORT, () => {
+  console.log(`API escuchando en http://localhost:${PORT}`);
+});
